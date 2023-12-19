a87fca80e9026d030a39035d2da43c42876dfa71
OrlandoRios90 <orlyr21@gmail.com>
2023-12-19 12:36:02 -0500

    work in progress, finished drawing the hangman

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -5,11 +5,33 @@ const BODY = (
     <div className="body" />
 )
 
+const RIGHT_ARM = (
+    <div className="right-arm" />
+)
+
+const LEFT_ARM = (
+    <div className="left-arm" />
+)
+
+const LEFT_LEG = (
+    <div className="left-leg" />
+)
+
+const RIGHT_LEG = (
+    <div className="right-leg" />
+)
+
+
 export function HangmanDrawing() {
     return <div className="hangman-drawing-container">
 
         {HEAD}
         {BODY}
+        {RIGHT_ARM}
+        {LEFT_ARM}
+        {LEFT_LEG}
+        {RIGHT_LEG}
+    
         <div className="rope" />
         <div className="top-bar" />
         <div className="pole" />
